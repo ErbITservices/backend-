e2a40ae30b5ed71106d715cdcf4c552d50b4dee1
ErbITservices <erbitservices@gmail.com>
2024-08-04 14:15:22 +0530

    b

--- db.js
+++ db.js
@@ -2,14 +2,15 @@ const mongoose= require('mongoose');
 
 // const mongoURL =`mongodb+srv://jayminprajapati5959:jaimin1902@cluster0.gsls1jf.mongodb.net/`
 
-const mongoURL='mongodb://localhost:27017/hotel'
+const mongoURL= process.env.DATA_BASE;
 const db = ()=>{
     
     mongoose.connect(mongoURL/*,{
       useNewUrlParser:true,
      useUnifiedTopology:true,
     }*/).then(()=>{
-       console.log( 'MongoDB Connected successfully...');   
+       console.log( 'MongoDB Connected successfully...');
+          
     }).catch((error)=>{
        console.log(error);
     })
